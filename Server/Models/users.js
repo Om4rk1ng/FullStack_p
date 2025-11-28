35aa9dfe67791ba5708767ab69e50319bd6e6655
Om4rk1ng <120275977+Om4rk1ng@users.noreply.github.com>
2025-11-28 21:35:52 +0400

    Update users.js

--- Server/Models/users.js
+++ Server/Models/users.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  Username: { type: String, required: true },
+  name: { type: String, required: true },
   Email: { type: String, required: true },
   Password: { type: String, required: true },
 });
 
 
-const User_model = mongoose.model("users", UserSchema, "users");
+const User_model = mongoose.model("userstable", UserSchema, "userstable");
 
 export default User_model;
