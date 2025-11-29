f071f6463103e381396ac6662c7d1f56a8e7367c
EssamDuude <EAlfoory@gmail.com>
2025-11-29 21:14:44 +0400

    Login / Register UI Done
    Hopefully nothing goes wrong  o(￣┰￣*)ゞ

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './comps/Login';
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Register from './comps/Registration';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. sdfsd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/register' element={<Register/>}/>
+
+    </Routes>
+    </BrowserRouter>
+  
+    
   );
 }
 
