73c204eb16b19533cedafe6625a2a04cddca5b1b
Majd <u19er123@gmail.com>
2025-12-11 22:41:25 +0400

    slice_1000

--- Client/src/features/slice.js
+++ Client/src/features/slice.js
@@ -15,7 +15,7 @@ export const RegisterDataThunk = createAsyncThunk(
 
 
     async (userData) => {
-    const serverENVURL=process.env.REACT_APP_
+    const serverENVURL=process.env.REACT_APP_CONNECTION
 const requestToPost = await Axios.post(`${serverENVURL}/register`, userData);
 
     return requestToPost.data;
@@ -36,7 +36,7 @@ export const LoginThunk = createAsyncThunk(
 
 
     async (userLoginData) => {
-          const serverENVURL=process.env.REACT_APP_
+          const serverENVURL=process.env.REACT_APP_CONNECTION
 
     const requestToPost = await Axios.post(
       `${serverENVURL}/login`,
